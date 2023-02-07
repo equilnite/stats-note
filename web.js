490f2094073b3e8e0b0370412b23f09cebe4a07d
equilnite <equilnite@gmail.com>
2023-02-07 10:49:49 -0800

    beta calculator

---
+++ web.js
@@ -0,0 +1,45 @@
+const history = [];
+
+var historyI = 0;
+
+const repl = document.querySelector('#repl');
+
+const replHist = document.querySelector('#repl-history');
+
+const command = document.querySelector('#command');
+
+repl.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        //const command = e.target.querySelector('#command').value;
+        const value = eval(command.innerHTML);
+        replHist.innerHTML += ('<span class="prompt" id="prompt">»&nbsp;</span>' + command.innerHTML) + '<br>' + (value === undefined ? 'undefined' : value) + '<br><br>';
+        history.push(command.innerHTML);
+        command.innerHTML = "";
+        repl.scrollTop = repl.scrollHeight;
+        historyI = history.length;
+    }
+});
+
+command.addEventListener('keydown', function (e) {
+    console.log(e);
+    if (e.key === 'ArrowUp') {
+       e.preventDefault();
+        if (historyI > 0) {
+            historyI -= 1;
+            command.innerHTML = history[historyI];
+        }
+    } else if (e.key === 'ArrowDown') {
+       e.preventDefault();
+        if (historyI < history.length - 1) {
+            historyI += 1;
+            command.innerHTML = history[historyI];
+        }
+    }
+});
+
+repl.addEventListener('onfocus',
+                     function(e) {
+                       console.log("focused!");
+                       cursor.focus();
+                     })
